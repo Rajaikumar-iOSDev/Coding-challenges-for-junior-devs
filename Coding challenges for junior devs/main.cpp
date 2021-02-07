cf46c41d96870afa6c0aa132a802e2dbc4ac4e91
Rajai Kumar <writetorajai@outlook.com>
2021-02-07 11:35:01 +0530

    Added code for Find the biggest and smallest number in the array & Maximum sum of an hour glass.

--- Coding challenges for junior devs/main.cpp
+++ Coding challenges for junior devs/main.cpp
@@ -49,12 +49,77 @@ static void handlePalindrome() {
     handleChoice();
 }
 
+// MARK:- Maximum sum of hour glass in matrix
+///Declare properties
+int array2D[5][5]={{1, 2, 3, 0, 0},
+    {0, 0, 0, 0, 0},
+    {2, 1, 4, 0, 0},
+    {0, 0, 0, 0, 0},
+    {1, 1, 0, 1, 0}};
+int rows = 5;
+int columns = 5;
+///Find max sum of the hour glass
+int maxSumOfHourGlassfor(int array[5][5])
+{
 
+    int maxSum = INT_MIN;
+    for(int i=0;i<rows-2;i++){
+        for (int j=0; j<columns-2; j++) {
+            int sum = array[i][j]+array[i][j+1]+array[i][j+2]+array[i+1][j+2]+array[i+2][j]+array[i+2][j+1]+array[i+2][j+2];
+            maxSum = max(maxSum,sum);
+        }
+    }
 
+    return maxSum;
+}
+
+static void handleMaxSumOfHourGlass(){
+
+    cout<<"Maximum sum of the hour glass : "<<maxSumOfHourGlassfor(array2D)<<"\n";
+    handleChoice();
+}
+
+// MARK:- Find the biggest and smallest number in the array
+///Find the smallest number
+int findSmallest(int *array,long length){
+
+    int smallestElement = array[0];
+    for (int i=0;i<length;i++){
+        if(array[i]<smallestElement){
+            smallestElement=array[i];
+        }
+    }
+    return smallestElement;
+}
+///Find the biggest number
+int findBiggest(int *array, long length){
+    int biggestElement = array[0];
+    for(int i=0;i<length;i++){
+        if (array[i]>biggestElement){
+            biggestElement = array[i];
+        }
+    }
+    return biggestElement;
+}
+///Handle input and output
+static void handleFindtheBiggestAndSmallestNumber(){
+
+    int array[5];
+
+    cout<<"Enter integer numbers to create an array: ";
+    for(int i=0;i<5;i++){
+        cin>>array[i];
+    }
+
+    int length = *(&array+1) - array;
+
+    cout<<"Biggest number in the given array is : "<<findBiggest(array, length)<<"\n";
+    cout<<"Smallest number in the given array is : "<<findSmallest(array, length)<<"\n";
+}
 // MARK:- Driver functions
 static void handleChoice() {
     int choice;
-    cout<<"Welcome to the 'Junior dev - Coding interview Questions & Answers - Part 1' \n\n Please choose an option to proceed:\n\n Press '1' for Factorial \n Press '2' for Palindrome\n Press any other number to exit\n";
+    cout<<"Welcome to the 'Junior dev - Coding interview Questions & Answers - Part 1' \n\n Please choose an option to proceed:\n\n Press '1' for Factorial \n Press '2' for Palindrome\n Press '3' for Max Sum Of Hour Glass\n Press '4' for Find the biggest and smallest number\n Press any other number to exit\n";
     cin>>choice;
 
     switch (choice) {
@@ -64,11 +129,17 @@ static void handleChoice() {
         case 2:
             handlePalindrome();
             break;
+        case 3:
+            handleMaxSumOfHourGlass();
+            break;
+        case 4:
+            handleFindtheBiggestAndSmallestNumber();
+            break;
         default:
             break;
     }
 }
-int main(int argc, const char * argv[]) {
+int main() {
     handleChoice();
     return 0;
 }
